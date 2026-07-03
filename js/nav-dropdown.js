a8a5ec91371ccc420fb1331465691b0f7726815a
Bipul Debnath <bdnatheduknow27@gmail.com>
2026-07-03 14:31:38 +0530

    Delete js directory

--- js/nav-dropdown.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.nav-item > span').forEach(item => {
-  item.addEventListener('click', e => {
-    e.preventDefault();
-    const dropdown = item.nextElementSibling;
-    dropdown.classList.toggle('open');
-  });
-});
